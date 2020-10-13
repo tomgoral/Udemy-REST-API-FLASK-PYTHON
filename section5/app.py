53c9321165e2c1891afdc1e2bf832b4a75c54462
Tom Goral <tg@tomgoral.com>
2020-10-13 14:15:13 -0400

    added some jwt_extended

--- section5/app.py
+++ section5/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+
 from security import authenticate, identity
 from user import UserRegister
 from item import Item, ItemList
--- techwithtim/test0.py
+++ techwithtim/test0.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.patch(BASE + "video/2", {})
+response = requests.patch(BASE + "video/2", {"views":99})
 print(response.json())
