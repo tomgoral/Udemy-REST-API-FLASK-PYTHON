57be7413fb542c1ec49ce8c01e2bc53a82403566
Tom Goral <tg@tomgoral.com>
2020-10-20 15:48:53 -0400

    completed s6

--- section6/app.py
+++ section6/app.py
@@ -7,6 +7,8 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
+from db import db  # this is new
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,7 +17,7 @@ app.secret_key = 'jose'
 api = Api(app)
 
 
-@app.before_first_request
+@app.before_first_request  # This will create tables before the first request
 def create_tables():
     db.create_all()
 
@@ -29,6 +31,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
+    #from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
